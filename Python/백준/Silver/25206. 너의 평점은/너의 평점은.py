b0ea6412de813f22e3b67d8dd1d2cd204b14005b
Kim SiWoo <91021927+Rlatldn18@users.noreply.github.com>
2026-03-22 03:30:11 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,21 @@
+score_dict = {
+    "A+" : 4.5,
+    "A0" : 4.0,
+    "B+" : 3.5,
+    "B0" : 3.0,
+    "C+" : 2.5,
+    "C0" : 2.0,
+    "D+" : 1.5,
+    "D0" : 1.0,
+    "F" : 0.0
+}
+credit_sum = 0
+creditgpa_sum = 0
+
+for _ in range(20):
+    sub, credit, gpa = input().split()
+    if gpa != "P":
+        credit_sum +=  float(credit)
+        creditgpa_sum += float(credit) * score_dict[gpa]
+
+print(creditgpa_sum/credit_sum)
