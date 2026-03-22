a7cf6b57bb767dcebfde71cb558af3012977b5ea
Kim SiWoo <91021927+Rlatldn18@users.noreply.github.com>
2026-03-23 02:27:03 +0900

    [Silver V] Title: 분수찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+#현재 줄 번호
+line = 1
+#현재 줄까지의 마지막 번호
+total = 1
+
+x = int(input())
+
+#x가 속한 줄 찾는것
+# total = 1 - 3 - 6 - 10 - ...
+while total < x:
+    line += 1       #다음 줄로 이동
+    total += line   #총 몇 개의 배열이있는지
+    
+#현재 줄은 찾은 상태 
+#현재 줄에서 몇 번째인지 찾기
+#현재 줄의 시작 번호 = total - line + 1
+idx = x - (total - line)
+
+#분자 / 분모 결정 
+#짝수 줄일때 홀수 줄일때 나눠짐
+if line % 2 == 0:   #짝수 줄일 때
+    up = idx
+    down = line - idx + 1
+
+else:
+    up = line - idx + 1
+    down = idx
+    
+print(f"{up}/{down}")
