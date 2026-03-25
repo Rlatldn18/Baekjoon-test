0c46241ac939e2574db4ab40ddd34e3bcdb973e7
Kim Siwoo <91021927+Rlatldn18@users.noreply.github.com>
2026-03-26 00:20:19 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 784 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ Python/백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,9 +1,19 @@
+'''
+    입력은 a b c d e f를 받아야함
+
+    연립일차방정식     
+    ax + by = c
+    dx + ey = f
+    이때 x y의 해는 무조건 1개
+    즉 기울기의 부호가 다름
+    ae != db
+'''
 import sys
 input = sys.stdin.readline
 
 a, b, c, d, e, f = map(int, input().split())
 
-for i in range(-999, 1000):
-    for j in range(-999, 1000):
-        if a*i + b*j == c and d*i + e*j == f:
-            print(f"{i} {j}")
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a*x + b*y == c and d*x + e*y == f:
+            print(f"{x} {y}")
