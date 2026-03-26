8e5af915f0cb6ef10a2dfd9c1879c38408144b18
Kim Siwoo <91021927+Rlatldn18@users.noreply.github.com>
2026-03-26 21:37:52 +0900

    [Bronze II] Title: 수 정렬하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+num_list = []
+for _ in range(n):
+    num = int(input())
+    num_list.append(num)
+    
+num_list.sort()
+
+print(*num_list, sep="\n")
