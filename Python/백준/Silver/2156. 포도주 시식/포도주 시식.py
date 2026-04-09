48d3c161c4bf284eb6f22c2f55494ef2994420cf
Kim Siwoo <91021927+Rlatldn18@users.noreply.github.com>
2026-04-09 22:45:49 +0900

    [Silver I] Title: 포도주 시식, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+wine = [0]*(n+1)
+
+for i in range(1, n+1):
+  wine[i] = int(input())
+
+if n == 1:
+    print(wine[1])
+else:
+    dp = [0]*(n+1)
+    dp[1] = wine[1]
+    dp[2] = wine[1] + wine[2]
+    
+    for i in range(3, n+1):
+        dp[i] = max(
+            dp[i-1],                        #i번째 잔 안마심
+            dp[i-2] + wine[i],              #i번째 잔만 마심
+            dp[i-3] + wine[i-1] + wine[i]   #i-1,i번째 잔 마심 
+        )
+    print(dp[n])
